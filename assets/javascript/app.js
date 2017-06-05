edcf86d98462b640b2b1045aa784f7ca5af9b0af
ccruml20 <ccrumley1@gmail.com>
2017-06-05 14:46:53 -0600

    styled css and improved app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,12 @@
+var clock;
+var counter = 16;
 var correct = 0;
 var incorrect = 0;
 var answerArray = [];
-var currentQuestion;
+var currentQuestion = 0;
+
+// question object array to set up bank of questions
+
 var questions = [{
     question: "Which of the great lakes is the largest freshwater lake in the world?",
     choices1: "Lake Michigan",
@@ -71,109 +76,100 @@ var questions = [{
     choices2: "Pawn",
     choices3: "King",
     choices4: "Knight",
-    correctAnswer: "Bishop",}, {
-}]
+    correctAnswer: "Bishop",
+}, {}]
 
-console.log(questions[5].choices);
-
-console.log(questions[5].correctAnswer);
-console.log(questions[2].choices3.correctAnswer);
-
+// Object that holds all of my functions
 
 var self = {
 
-initGame: function(){
-},
+        // Set up timer function to count down from 15 seconds
 
-currentQuestion: function(){
-    currentQuestion = 0;
-},
+        myTimer: function() {
 
+            clock = setInterval(fifteenSeconds, 1000);
 
-nextQuesion: function(){
-    $("#next").on("click", function(){
-        currentQuestion++;
-    self.generateHtml(); 
-    if (answerArray[currentQuestion] === true){
-        
-        correct++;
-        $("#correct").html("correct" + "" + correct);
-        
-    }   
-    else {
-        incorrect++;
-        currentQuestion++;
-        $("#incorrect").html("incorrect" + "" + incorrect);
-        
-    } 
+            function fifteenSeconds() {
+                if (counter === 0) {
+                    // timeOutLoss();
+                    clearInterval(clock);
+                    self.myTimer();
+                    $("#next").html("Next Question");
+                              }
+                if (counter > 0) {
+                    counter--;
+                }
+                $("#timer").html(counter);
+            }
+        },
 
+        //  function that iterates through the question array
 
-    })
+        currentQuestion: function() {
+            currentQuestion++;
+        },
 
-},
+        //  button function that generates the html and updates the wins and loses
 
+        nextQuesion: function() {
+            $("#next").on("click", function() {
+                $("#next").html("Confirm Answer");
+                counter = 16;
+                self.generateHtml();
+                if (answerArray[currentQuestion] === true) {
+                    correct++;
+                    $("#correct").html(correct);
+                    console.log(currentQuestion + "bla")
+                    self.currentQuestion();
+                    self.generateHtml();
+                } else {
+                    incorrect++;
+                    $("#incorrect").html(incorrect);
+                    self.currentQuestion();
+                    self.generateHtml();
 
+                }
+            })
+        },
 
-generateHtml: function(){
-    $(".questionField").html(questions[currentQuestion].question);
-    console.log("hey" + questions[currentQuestion].question);
-    $("#answer").html(questions[currentQuestion].choices1);
-    $("#answer1").html(questions[currentQuestion].choices2);
-    $("#answer2").html(questions[currentQuestion].choices3);
-    $("#answer3").html(questions[currentQuestion].choices4);
-    console.log(questions[currentQuestion].choices1);
-},
+        // function for generating the HTML
 
-}
+        generateHtml: function() {
+            $(".questionField").html(questions[currentQuestion].question);
+            console.log("hey" + questions[currentQuestion].question);
+            $("#answer").html(questions[currentQuestion].choices1);
+            $("#answer1").html(questions[currentQuestion].choices2);
+            $("#answer2").html(questions[currentQuestion].choices3);
+            $("#answer3").html(questions[currentQuestion].choices4);
+            console.log(questions[currentQuestion].choices1);
+        },
 
+    }
+    // Event listers for on click that compares the question and answers and turns it into 
+    // a boolean value within an if statement.  Then push result to an array.   
 
 document.addEventListener("DOMContentLoaded", function() {
 
 
-    
-    $(".answerField").on("click", function(){
+
+    $(".answerField").on("click", function() {
         var answerPick = $(this).text()
-        console.log("Im the answerP",answerPick.trim() + "Im the question",questions[currentQuestion].correctAnswer)
-        if (answerPick === questions[currentQuestion].correctAnswer){
-            answerArray[currentQuestion]= true;
-             console.log("yeah");
-        }
-        else {
-            answerArray[!currentQuestion]= false;
+        console.log("Im the answerP", answerPick + "Im the question", questions[currentQuestion].correctAnswer)
+        if (answerPick === questions[currentQuestion].correctAnswer) {
+            answerArray[currentQuestion] = true;
+            console.log("yeah");
+        } else {
+            answerArray[currentQuestion] = false;
             // $("#incorrect").html(incorrect);
 
         }
         console.log(answerArray);
     })
 
+    // Document loading function calls 
 
-
-    // self.initGame()
-    self.currentQuestion()
+    self.myTimer()
     self.nextQuesion()
     self.generateHtml()
 
-    // self.updateStats()
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
